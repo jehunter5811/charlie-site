ed44813def90740bd85e45e5369fc968c846bedf
jehunter5811 <justin.edward.hunter@gmail.com>
2019-01-17 05:43:58 -0600

    Added headers and redirects file

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -34,7 +34,7 @@ class Home extends Component {
         </div>
 
 
-        <div id='pubs' className="container">
+        <div id='pubs' className="container" style={{marginBottom: "40px"}}>
           <div className="section">
             <h2>Publications</h2>
             <div className="row">
@@ -55,44 +55,6 @@ class Home extends Component {
             </div>
 
           </div>
-        </div>
-
-
-        <div className="parallax-container valign-wrapper">
-          <div className="section no-pad-bot">
-            <div className="container">
-              <div className="row center">
-                <h5 className="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
-              </div>
-            </div>
-          </div>
-          <div className="parallax"><img src={background2} alt="Unsplashed background img 2" /></div>
-        </div>
-
-        <div className="container">
-          <div className="section">
-
-            <div className="row">
-              <div className="col s12 center">
-                <h3><i className="mdi-content-send brown-text"></i></h3>
-                <h4>Contact Us</h4>
-                <p className="left-align light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque id nunc nec volutpat. Etiam pellentesque tristique arcu, non consequat magna fermentum ac. Cras ut ultricies eros. Maecenas eros justo, ullamcorper a sapien id, viverra ultrices eros. Morbi sem neque, posuere et pretium eget, bibendum sollicitudin lacus. Aliquam eleifend sollicitudin diam, eu mattis nisl maximus sed. Nulla imperdiet semper molestie. Morbi massa odio, condimentum sed ipsum ac, gravida ultrices erat. Nullam eget dignissim mauris, non tristique erat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;</p>
-              </div>
-            </div>
-
-          </div>
-        </div>
-
-
-        <div className="parallax-container valign-wrapper">
-          <div className="section no-pad-bot">
-            <div className="container">
-              <div className="row center">
-                <h5 className="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
-              </div>
-            </div>
-          </div>
-          <div className="parallax"><img src={background3} alt="Unsplashed background img 3" /></div>
         </div>
       </div>
     );
